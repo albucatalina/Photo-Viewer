2a412e3203ba504f8afe39893d3e513a0f42bd0e
Albu Catalina <87013834+albucatalina@users.noreply.github.com>
2024-04-27 16:05:20 +0300

    Photo view layout
    Photo view layout that contains the title and 1photo with description

--- app/src/main/java/com/example/photoviewer/MainActivity.kt
+++ app/src/main/java/com/example/photoviewer/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.photoviewer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.photoviewer.ui.theme.PhotoViewerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +39,77 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    PhotoViewerLayout()
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun PhotoViewerLayout(){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(horizontal = 24.dp)
+                .padding(top = 80.dp)
+                .fillMaxSize()
+        ) {
+            Title(
+                title = "MILAN LANDMARKS",
+                modifier = Modifier
+                    .padding(vertical = 30.dp)
+            )
+            PhotoWall(
+                image = R.drawable.arco_della_pace,
+                description = "Arco della Pace",
+                address = "address",
+                modifier = Modifier
+                    .padding(horizontal = 24.dp)
+                    .background(Color.White)
+                    .padding(bottom = 20.dp)
+            )
+        }
+    }
+
+    @Composable
+    private fun Title(title: String, modifier: Modifier = Modifier){
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = modifier
+        )
+    }
+    @Composable
+    private fun PhotoWall(
+        @DrawableRes image: Int,
+        description: String,
+        address: String,
+        modifier: Modifier = Modifier
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+        ) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = description,
+                modifier = Modifier.padding(24.dp)
+            )
+            Text(
+                text = description,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+            Text(text = address)
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PhotoViewLayoutPreview(){
+        PhotoViewerTheme {
+            PhotoViewerLayout()
         }
     }
 }
