413004b520fbe311970290b1c0eb4e279e4c8c43
Albu Catalina <87013834+albucatalina@users.noreply.github.com>
2024-04-28 09:45:18 +0300

    Update PhotoWall Layout
    -Added a shadow to the picture
    -Change the background inside PhototWall function
    -Remove unused imports

--- app/src/main/java/com/example/photoviewer/MainActivity.kt
+++ app/src/main/java/com/example/photoviewer/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.example.photoviewer
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -26,14 +22,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
@@ -98,7 +93,6 @@ class MainActivity : ComponentActivity() {
                 description = pictures[pictureIndex].description,
                 address = pictures[pictureIndex].address,
                 modifier = Modifier
-                    .background(Color.White)
                     .padding(bottom = 8.dp)
             )
             Box(
@@ -136,6 +130,8 @@ class MainActivity : ComponentActivity() {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = modifier
+                .shadow(10.dp)
+                .background(Color.White)
         ) {
             Image(
                 painter = painterResource(id = image),
